cbae4a819da9b67504337cfb67068ad9a21a0f9c
YongYong21 <imkinggosu@gmail.com>
2023-08-12 23:39:52 +0900

    Debug: remove employee

--- asset/js/employee.js
+++ asset/js/employee.js
@@ -24,12 +24,12 @@ let allKeys = Object.keys(localStorage);
 const visibleEmployees = 10; // 초기에 보이는 직원 수
 const additionalEmployees = 3; // 스크롤링할 때 추가로 보여지는 직원 수
 let lastVisibleEmployeeIndex = visibleEmployees - 1; // 마지막으로 보이는 직원 인덱스
-let allEmployeesLoaded = false; 
+let allEmployeesLoaded = false;
 // 초기에 보이는 직원 목록 리스팅
 listEmployees(0, lastVisibleEmployeeIndex);
 
 // 스크롤 이벤트 리스너 추가
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (allEmployeesLoaded) {
     return;
   }
@@ -45,15 +45,17 @@ window.addEventListener('scroll', () => {
 function listEmployees(startIndex, endIndex) {
   for (let i = startIndex; i <= endIndex; i++) {
     const key = allKeys[i];
-    if (key === 'hasExecuted') {
+    if (key === "hasExecuted") {
       continue;
     }
     const storedUserInfo = localStorage.getItem(key);
-    const userInfo = JSON.parse(storedUserInfo);
+
+    if (storedUserInfo !== null){
+      const userInfo = JSON.parse(storedUserInfo);
 
     const userList = `
-    <tr>
-    <td class='check'><input type="checkbox"></td>
+    <tr data-name="${userInfo.name}">
+    <td class='check'><input type="checkbox" ></td>
     <td><img src="" id="${userInfo.key}"></td>
     <td><span>${userInfo.name}</span></td>
     <td><span>${userInfo.email}</span></td>
@@ -61,14 +63,13 @@ function listEmployees(startIndex, endIndex) {
     <td><span>${userInfo.classification}</span></td>
     <td><button class='profile-btn'>프로필 보기</button></td>
     </tr>`;
-      $("tbody").append(userList);
+    $("tbody").append(userList);
 
-      
-      if(userInfo.hasImage){
-        getDownloadURL(ref(storage, `image/${userInfo.key}`))
+    if (userInfo.hasImage) {
+      getDownloadURL(ref(storage, `image/${userInfo.key}`))
         .then((url) => {
           // `url` is the download URL for 'images/stars.jpg'
-    
+
           // This can be downloaded directly:
           // const xhr = new XMLHttpRequest();
           // xhr.responseType = "blob";
@@ -83,35 +84,35 @@ function listEmployees(startIndex, endIndex) {
           img.attr("src", url);
         })
         .catch((error) => {
-          console.log('실패')
+          console.log("실패");
         });
-      }
-      else {
-        getDownloadURL(ref(storage, `image/default.png`))
+    } else {
+      getDownloadURL(ref(storage, `image/default.png`))
         .then((url) => {
           const img = $(`#${userInfo.key}`);
           img.attr("src", url);
-          
         })
         .catch((error) => {
-          console.log('실패')
+          console.log("실패");
         });
-      }
+    }
+    }
+    
   }
   $(".profile-btn").on("click", function (event) {
     const row = $(this).closest("tr");
-  
+
     const userName = row.find("td:nth-child(3) span").text();
     const userEmail = row.find("td:nth-child(4) span").text();
     const userPhone = row.find("td:nth-child(5) span").text();
     const userClassification = row.find("td:nth-child(6) span").text();
-  
+
     const profileUrl = `profile.html?name=${encodeURIComponent(
       userName
     )}&email=${encodeURIComponent(userEmail)}&phone=${encodeURIComponent(
       userPhone
     )}&classification=${encodeURIComponent(userClassification)}`;
-  
+
     window.location.href = profileUrl;
   });
 }
@@ -125,7 +126,7 @@ function loadMoreEmployees() {
     return;
   }
   // 추가 데이터를 가져와서 리스팅
-  listEmployees( 
+  listEmployees(
     lastVisibleEmployeeIndex - additionalEmployees + 1,
     lastVisibleEmployeeIndex
   );
@@ -149,7 +150,6 @@ function loadMoreEmployees() {
 // <td><button class='profile-btn'>프로필 보기</button></td>
 // </tr>`;
 //   $("tbody").append(userList);
-
 
 //   if(userInfo.hasImage){
 //     getDownloadURL(ref(storage, `image/${userInfo.key}`))
@@ -177,7 +177,7 @@ function loadMoreEmployees() {
 //     .then((url) => {
 //       const img = $(`.${userInfo.name} #userimg`);
 //       img.attr("src", url);
-      
+
 //     })
 //     .catch((error) => {
 //       console.log('실패')
@@ -186,31 +186,42 @@ function loadMoreEmployees() {
 
 // });
 
-$(".remove-btn").on("click", (e) => {
+$(".remove-btn").on("click", () => {
+  const promises = [];
+
   allKeys.forEach((key) => {
     let storedUserInfo = localStorage.getItem(key);
-
     let userInfo = JSON.parse(storedUserInfo);
 
-    if ($(`.${userInfo.name} input`).prop("checked")) {
+    if ($(`tr[data-name="${userInfo.name}"] input`).prop("checked")) {
       const storage = getStorage();
-
-      // Create a reference to the file to delete
       const desertRef = ref(storage, `image/${userInfo.key}`);
 
-      // Delete the file
-      deleteObject(desertRef)
+      
+      const removePromise = deleteObject(desertRef)
         .then(() => {
-          // File deleted successfully
+          console.log('파일 삭제 성공');
         })
-        .catch((error) => {});
+        .catch((error) => {
+          console.log('데이터 삭제 오류', error);
+        });
 
       localStorage.removeItem(key);
+
+      promises.push(removePromise);
     }
   });
-  window.location.href = "index.html";
-});
 
+  
+  Promise.all(promises)
+    .then(() => {
+      console.log('삭제 완료');
+      window.location.href = "index.html";
+    })
+    .catch((error) => {
+      console.log('삭제 실패', error);
+    });
+});
 
 // // 프로필 버튼 클릭 이벤트 핸들러
 // $(".profile-btn").on("click", function (event) {
